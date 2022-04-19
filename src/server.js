b2dc09886ca60df607a7d7bb45f8ba974c5dba1b
danial <dpezeshpoor@yahoo.com>
2022-04-19 11:47:39 +0430

    server first init commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+//third-party packages and libs 
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+//server and database configurations
+const server = express();
+const port = process.env.PORT;
+const hostName = process.env.HOST_NAME; 
+const DBUri = process.env.DB_URI;
+
+//applying middlewares
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
+
+//importing section for routers  
+
+
+//setting server and database alive
+mongoose.connect(DBUri, {
+    useNewUrlParser: true
+})
+    .then(() => {server.listen(port, hostName, () => console.log(`server is alive at $http://${hostName}:${port}`))})
+    .catch(error => {console.log(error)});
+
