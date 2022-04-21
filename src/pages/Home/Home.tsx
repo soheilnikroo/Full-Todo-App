781eaa36f98554154ab500625a37e8c9c6bdcdd0
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-21 17:24:42 +0430

    change default home screen

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,25 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage className="test">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <p style={{ color: '#000' }}>skjskksjk</p>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
