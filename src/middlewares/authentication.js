dde89e7312445c65dadefd47533d7c6d2184fa66
danial <dpezeshpoor@yahoo.com>
2022-04-21 12:05:41 +0430

    authentication middleware has been implemented

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,30 @@
+//third-party packages and libs
+const jwt = require('jsonwebtoken');
+
+//User model
+const User = require('../models/User');
+
+const authenticate = async (req, res, next) => {
+    try{
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findOne({_id: decodedToken._id, 'tokens.token': token}); 
+
+        if(!user){
+            throw new Error('')
+        };
+
+        req.user = user;
+        req.token = token;
+
+        next();
+    }catch(error){
+        res.status(401).json({
+            error: 'Please authenticate first!'
+        });
+    };
+};
+
+//exporting section 
+module.exports = authenticate;
