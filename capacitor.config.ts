ea20f4c0d7ee3cfe1a7208a9743db868a8951595
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-21 17:20:48 +0430

    change capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'full-todo-app',
+  appId: 'com.sharex-todo.app',
+  appName: 'Todo',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
