bffd49279e405b898e863ec67c6600fed407570c
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-25 19:21:50 +0430

    export newly added components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
 export { default as ForwardButton } from './Buttons/ForwardButton/ForwardButton';
+export { default as BackButton } from './Buttons/BackButton/BackButton';
+export { default as PrimaryButton } from './Buttons/PrimaryButton/PrimaryButton';
+
+export { default as InputField } from './InputField/InputField';
+export { default as EmailInput } from './InputField/EmailField/EmailField';
+export { default as PasswordInput } from './InputField/PasswordField/PasswordField';
 
 export { default as Logo } from './Logo/Logo';
