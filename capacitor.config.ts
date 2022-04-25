ad95af35c3e5f74054910ee8e8d41f853b7b8a17
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-25 19:14:06 +0430

    added splash screen cpacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,23 @@ const config: CapacitorConfig = {
   appName: 'Todo',
   webDir: 'build',
   bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#382AEE',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
