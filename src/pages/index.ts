d3bce8f5b214059131c8538c0d47752fc95f7c39
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-25 19:19:38 +0430

    export LoginPage component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Home } from './Home/Home';
 export { default as WelcomePage } from './Welcome/WelcomePage';
+export { default as LoginPage } from './AuthenticationPages/LoginPage/LoginPage';
