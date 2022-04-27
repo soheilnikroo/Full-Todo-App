91da364623058814ee1c42b4fe4ff59b4060634d
Soheil Nikroo <soheiln1234@gmail.com>
2022-04-27 21:26:11 +0430

    export sign in page from page folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home/Home';
 export { default as WelcomePage } from './Welcome/WelcomePage';
 export { default as LoginPage } from './AuthenticationPages/LoginPage/LoginPage';
+export { default as SigninPage } from './AuthenticationPages/SigninPage/SigninPage';
