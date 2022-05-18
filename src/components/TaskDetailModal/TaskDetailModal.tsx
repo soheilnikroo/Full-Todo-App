c7b449074b52c95696b7700a607c503525975a6a
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:16:13 +0430

    cerate task detail  component with style

---
+++ src/components/TaskDetailModal/TaskDetailModal.tsx
@@ -0,0 +1,282 @@
+import {
+  InputChangeEventDetail,
+  IonContent,
+  IonIcon,
+  IonInput,
+  IonModal,
+  IonText,
+  IonTextarea,
+  IonToggle,
+  TextareaChangeEventDetail,
+  ToggleChangeEventDetail,
+} from '@ionic/react';
+import React, { useEffect, useReducer, useState } from 'react';
+import {
+  QueryObserverResult,
+  RefetchOptions,
+  RefetchQueryFilters,
+} from 'react-query';
+import { Todo } from '../../models';
+import PrimaryButton from '../Buttons/PrimaryButton/PrimaryButton';
+
+import { closeOutline as closeIcon, trashOutline } from 'ionicons/icons';
+
+import classes from './style/TaskDetailModal.module.css';
+import { useDeleteTask, useGetTask } from '../../hooks';
+import useUpdateTask from '../../hooks/useUpdateTask';
+
+interface TaskDetailModalProps {
+  taskId: string;
+  isOpen: boolean;
+  reFetch: <TPageData>(
+    options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
+  ) => Promise<QueryObserverResult<Todo[], unknown>>;
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+enum ActionTypes {
+  SET_ALL_FIELDS = 'SET_ALL_FIELDS',
+  SET_TITLE = 'SET_TITLE',
+  SET_DESCRIPTION = 'SET_DESCRIPTION',
+  SET_DONE = 'SET_DONE',
+}
+
+interface Action {
+  type: ActionTypes;
+  payload: any;
+}
+
+const initialState = {
+  title: '',
+  description: '',
+  isDone: false,
+};
+
+const taskReducer = (
+  state: {
+    title: string;
+    description: string;
+    isDone: boolean;
+  },
+  action: Action
+) => {
+  switch (action.type) {
+    case ActionTypes.SET_ALL_FIELDS:
+      return {
+        ...state,
+        title: action.payload.title,
+        description: action.payload.description,
+        isDone: action.payload.isDone,
+      };
+    case ActionTypes.SET_TITLE:
+      return { ...state, title: action.payload.title };
+    case ActionTypes.SET_DESCRIPTION:
+      return { ...state, description: action.payload.description };
+    case ActionTypes.SET_DONE:
+      return { ...state, isDone: action.payload.isDone };
+    default:
+      return state;
+  }
+};
+let task: Todo | undefined;
+
+const TaskDetailModal: React.FC<TaskDetailModalProps> = ({
+  taskId,
+  reFetch,
+  isOpen,
+  setShowModal,
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const deletTask = useDeleteTask();
+  const { todosData, todosIsLoading, todosRefetch } = useGetTask();
+  const updateTask = useUpdateTask();
+
+  const [enableEditing, setEnableEditing] = useState(true);
+  const [selectedTask, dispatch] = useReducer(taskReducer, initialState);
+
+  useEffect(() => {
+    if (!todosIsLoading && todosData) {
+      task = todosData.find((todo: Todo) => todo._id === taskId);
+      dispatch({
+        type: ActionTypes.SET_ALL_FIELDS,
+        payload: task || initialState,
+      });
+    }
+  }, [todosData, taskId]);
+
+  const handleChangeTaskTitle = (
+    event: CustomEvent<InputChangeEventDetail>
+  ) => {
+    if (event.detail.value) {
+      dispatch({
+        type: ActionTypes.SET_TITLE,
+        payload: { title: event.detail.value },
+      });
+    }
+  };
+
+  const enableTitleEditing = () => {
+    setEnableEditing(false);
+  };
+
+  const disableTitleEditing = () => {
+    setEnableEditing(true);
+  };
+
+  const handleChangeTaskDescription = (
+    event: CustomEvent<TextareaChangeEventDetail>
+  ) => {
+    if (event.detail.value) {
+      dispatch({
+        type: ActionTypes.SET_DESCRIPTION,
+        payload: { description: event.detail.value },
+      });
+    }
+  };
+
+  const handleChangeIsCompleted = (
+    event: CustomEvent<ToggleChangeEventDetail<any>>
+  ) => {
+    dispatch({
+      type: ActionTypes.SET_DONE,
+      payload: { isDone: event.detail.checked },
+    });
+  };
+
+  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (isTaskChanged()) {
+      setIsLoading(true);
+      const newTodo = new Todo({
+        _id: taskId,
+        title: selectedTask.title,
+        description: selectedTask.description,
+        isDone: selectedTask.isDone,
+      });
+
+      updateTask.mutate(newTodo);
+    } else {
+      setShowModal(false);
+    }
+  };
+
+  const handleDeleteTask = () => {
+    setIsLoading(true);
+    deletTask.mutate(taskId);
+  };
+
+  useEffect(() => {
+    if (updateTask.isSuccess && !updateTask.isLoading) {
+      reFetch().then(() => {
+        setIsLoading(false);
+        setShowModal(false);
+      });
+    }
+    if (deletTask.isSuccess && !deletTask.isLoading) {
+      reFetch().then(() => {
+        setIsLoading(false);
+        setShowModal(false);
+      });
+    }
+  }, [
+    updateTask.isSuccess,
+    deletTask.isSuccess,
+    updateTask.isLoading,
+    deletTask.isLoading,
+  ]);
+
+  const isTaskChanged = () => {
+    if (
+      selectedTask.title !== task?.title ||
+      selectedTask.description !== task?.description ||
+      selectedTask.isDone !== task?.isDone
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <IonModal
+      animated
+      keyboardClose
+      isOpen={isOpen}
+      className={classes['modal']}
+    >
+      <IonContent className={classes['modal-content']}>
+        <section className={classes['modal-wrapper']}>
+          <div className={classes['modal-clos-wrapper']}>
+            <IonIcon
+              onClick={handleDeleteTask}
+              className={classes['delete-icon']}
+              icon={trashOutline}
+            />
+            <div
+              role="button"
+              onClick={() => {
+                setShowModal(false);
+              }}
+              className={classes['modal-close']}
+            >
+              <IonIcon icon={closeIcon} />
+            </div>
+          </div>
+          <form className={classes['modal-form']} onSubmit={formSubmitHandler}>
+            <div className={classes['modal-form-content']}>
+              <div
+                onDoubleClick={enableTitleEditing}
+                className={classes['modal-header']}
+              >
+                <IonInput
+                  onIonChange={handleChangeTaskTitle}
+                  className={classes['modal-title']}
+                  value={selectedTask.title}
+                  readonly={enableEditing}
+                  onBlur={disableTitleEditing}
+                />
+                <IonText className={classes['modal-subtitle']}>
+                  Description
+                </IonText>
+              </div>
+              <div className={classes['input-wrapper']}>
+                <IonTextarea
+                  mode="ios"
+                  placeholder="Task Description"
+                  wrap="soft"
+                  spellcheck={true}
+                  rows={6}
+                  className={classes['desc-area']}
+                  value={selectedTask.description}
+                  onIonChange={handleChangeTaskDescription}
+                ></IonTextarea>
+              </div>
+              <div className={classes['modal-switch']}>
+                <IonText className={classes['switch-title']}>
+                  isCompleted
+                </IonText>
+                <IonToggle
+                  checked={selectedTask.isDone}
+                  onIonChange={handleChangeIsCompleted}
+                  className={classes['switch-toggle']}
+                />
+              </div>
+            </div>
+            <div className={classes['button-wrapper']}>
+              <div className={classes['button']}>
+                <PrimaryButton
+                  disabled={isLoading}
+                  isLoading={isLoading}
+                  type="submit"
+                  text="Submit"
+                />
+              </div>
+            </div>
+          </form>
+        </section>
+      </IonContent>
+    </IonModal>
+  );
+};
+
+export default TaskDetailModal;
