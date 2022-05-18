8561f99bd10f21e509f6ea4b2ac5647e9da054e4
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:19:01 +0430

    expoty newly added custom hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,5 @@
 export { default as useForm } from './useForm';
+export { default as useGetTask } from './useGetTask';
+export { default as useGetUser } from './useGetUser';
+export { default as useAddTask } from './useAddTask';
+export { default as useDeleteTask } from './useDeleteTask';
