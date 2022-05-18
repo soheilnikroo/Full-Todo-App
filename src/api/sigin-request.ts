0c7eabac43bfba23ed8633b927541031a0233f54
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:05:28 +0430

    create signin request instanse of axios

--- src/api/sigin-request.ts
+++ src/api/sigin-request.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { BASE_URL } from './baseUrl';
 
 // Set config defaults when creating the instance
 const signinRequest = axios.create({
-  baseURL: 'http://localhost:5000/api/users/signup',
+  baseURL: `${BASE_URL}/users/signup`,
 });
 
 export default signinRequest;
