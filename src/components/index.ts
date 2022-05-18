f6ea64fb3475ee4006b0f2d3c63c8917efa87228
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:17:25 +0430

    export new component added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,27 @@
 export { default as ForwardButton } from './Buttons/ForwardButton/ForwardButton';
 export { default as BackButton } from './Buttons/BackButton/BackButton';
 export { default as PrimaryButton } from './Buttons/PrimaryButton/PrimaryButton';
+export { default as MenuButton } from './Buttons/MenuButton/MenuButton';
+export { default as AddTaskButton } from './Buttons/AddTaskButton/AddTaskButton';
 
 export { default as InputField } from './InputField/InputField';
 export { default as EmailInput } from './InputField/EmailField/EmailField';
 export { default as PasswordInput } from './InputField/PasswordField/PasswordField';
 
+export { default as CategoryList } from './Category/CategoriesList/CategoriesList';
+
+export { default as TodoSection } from './TodoSection/TodoSection';
+export { default as ShowTodos } from './ShowTodos/ShowTodos';
+
 export { default as Loader } from './Loader/Loader';
 
 export { default as Logo } from './Logo/Logo';
+
+export { default as Notification } from './Notification/Notification';
+export { default as Search } from './Search/Search';
+
+export { default as Header } from './Header/Header';
+
+export { default as Refresher } from './Refresher/Refresher';
+
+export { default as SideMenu } from './SideMenu/SideMenu';
