7cc6d5b178155e84fa846c08f4f0970f8f3c1bfb
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:04:39 +0430

    create get task request instanse of axios

---
+++ src/api/get-tasks.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import Todo from '../models/todos-model';
+import { TaskType } from '../types';
+import { BASE_URL } from './baseUrl';
+
+const getTasksRequest = async (access_token: string) => {
+  const response = await axios.get(`${BASE_URL}/tasks`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  if (response.data.length === 0) {
+    return response.data;
+  } else {
+    const data: Todo[] = response.data.tasks.map((task: TaskType) => {
+      return new Todo(task);
+    });
+
+    return data;
+  }
+};
+
+export default getTasksRequest;
