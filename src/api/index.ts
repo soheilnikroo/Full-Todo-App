41cffc5f08ac8302f5a58f6ab84b99896b888950
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:06:08 +0430

    export api request instance of axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,7 @@
 export { default as loginRequest } from './login-request';
 export { default as signinRequest } from './sigin-request';
+
+export { default as getTasksRequest } from './get-tasks';
+export { default as getRequestUser } from './get-user';
+export { default as postRequestTask } from './post-task';
+export { default as deleteRequestTask } from './delete-task';
