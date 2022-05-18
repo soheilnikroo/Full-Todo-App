5acbb8cf6d288a35aa3326bb57815c899cb89f2c
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:17:44 +0430

    export created context

---
+++ src/context/index.ts
@@ -0,0 +1,3 @@
+export { default as AuthProvider } from './auth-context';
+export { default as SearchProvider } from './search-context';
+export { default as IsTaskDraggingProvider } from './is-task-dragging';
