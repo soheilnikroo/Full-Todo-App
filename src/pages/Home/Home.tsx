c041220ca5f5ca24768e7ca56c8eb1c4b3bbe99a
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-18 13:20:57 +0430

    create home page component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,126 @@
 import {
   IonContent,
-  IonHeader,
   IonPage,
-  IonTitle,
-  IonToolbar,
+  IonText,
+  IonToast,
+  RefresherEventDetail,
 } from '@ionic/react';
-import './style/Home.css';
+import { Fragment, useContext, useEffect, useState } from 'react';
+import {
+  AddTaskButton,
+  CategoryList,
+  Header,
+  Refresher,
+  ShowTodos,
+  SideMenu,
+} from '../../components';
 
-const Home: React.FC = () => {
+// import css
+import classes from './style/Home.module.css';
+import AddTaskModal from '../../components/AddTaskModal/AddTaskModal';
+import { FilterTasks } from '../../util';
+import { useGetTask, useGetUser } from '../../hooks';
+import { Todo } from '../../models';
+import { IsTaskDraggingContext } from '../../context/is-task-dragging';
+
+const HomePage: React.FC = () => {
+  // username and tasks variables
+  let username: string = 'My Friend';
+
+  let todos: (Todo | undefined)[] = [];
+  let doneTodos: (Todo | undefined)[] = [];
+
+  const { isDrag } = useContext(IsTaskDraggingContext);
+  const [showModal, setShowModal] = useState(false);
+
+  const [warningToast, setWarningToast] = useState({
+    show: false,
+    message: '',
+  });
+
+  const { userData, userIsError, userIsLoading } = useGetUser();
+  const { todosData, todosError, todosIsError, todosIsLoading, todosRefetch } =
+    useGetTask();
+
+  if (!userIsLoading && !userIsError && userData) {
+    username = userData.userName;
+  }
+
+  if (!todosIsLoading && !todosIsError && todosData) {
+    todos = FilterTasks.filterTodos(todosData, FilterTasks.Filter.NotCompleted);
+    doneTodos = FilterTasks.filterTodos(
+      todosData,
+      FilterTasks.Filter.completed
+    );
+  }
+
+  const showAddTaskModal = () => {
+    setShowModal(true);
+  };
+
+  const [category, setCategory] = useState('todo');
+
+  const changeCategoryHandler = (categoryTitle: string): void => {
+    setCategory(categoryTitle);
+  };
+
+  function doRefresh(event: CustomEvent<RefresherEventDetail>) {
+    todosRefetch().then(() => {
+      event.detail.complete();
+    });
+  }
+
   return (
-    <IonPage className="test">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <p style={{ color: '#000' }}>skjskksjk</p>
-      </IonContent>
-    </IonPage>
+    <Fragment>
+      <IonToast
+        isOpen={warningToast.show}
+        onDidDismiss={() => setWarningToast({ show: false, message: '' })}
+        message={warningToast.message}
+        position="top"
+        color="danger"
+      />
+      <AddTaskModal
+        reFetch={todosRefetch}
+        isOpen={showModal}
+        setShowModal={setShowModal}
+      />
+      <SideMenu />
+      <IonPage id="home">
+        <Header />
+        <IonContent fullscreen>
+          <section className={classes['home']}>
+            <Refresher disabled={isDrag} onRefresh={doRefresh} />
+            <div className={classes['content-container']}>
+              <IonText className={classes['title']}>
+                What's up, {username}!
+              </IonText>
+              <div className={classes['category-container']}>
+                <IonText className={classes['category-title']}>
+                  Categories
+                </IonText>
+                <CategoryList
+                  loading={false}
+                  doneLength={doneTodos.length}
+                  taskLength={todos.length}
+                  onClick={changeCategoryHandler}
+                />
+              </div>
+              <ShowTodos
+                category={category}
+                todosData={todos}
+                doneData={doneTodos}
+                isLoading={todosIsLoading}
+              />
+              <AddTaskButton
+                isLoading={todosIsLoading || userIsLoading}
+                onClick={showAddTaskModal}
+              />
+            </div>
+          </section>
+        </IonContent>
+      </IonPage>
+    </Fragment>
   );
 };
 
-export default Home;
+export default HomePage;
