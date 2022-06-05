6951a171d50ec30705cc362d08784f5b3f834149
Soheil Nikroo <soheiln1234@gmail.com>
2022-06-05 14:50:10 +0430

    export setimage page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,7 @@ export { default as Home } from './Home/Home';
 export { default as WelcomePage } from './Welcome/WelcomePage';
 export { default as LoginPage } from './AuthenticationPages/LoginPage/LoginPage';
 export { default as SigninPage } from './AuthenticationPages/SigninPage/SigninPage';
+export { default as SetImagePage } from './SetImagePage/SetImagePage';
 export { default as ProfilePage } from './ProfilePage/ProfilePage';
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
 export { default as ErrorPage } from './ErrorPage/ErrorPage';
